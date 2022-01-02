5c5ed24a9df3bd549a86dc82c3c4aa1cc7a96c8d
hossamElgainy <7osoelganint@gmail.com>
2022-01-02 22:46:34 +0200

    Add  the Stack Implementation

---
+++ DataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(5);
+            s.Push(10);
+            s.Push(15);
+            s.Push(20);
+            s.Print();
+
+            s.Pop();
+            s.Push(7);
+            s.Print();
+        }
+    }
+}
---
+++ DataStructure/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    class Stack
+    {
+        private int top;
+        int[] item=new int[100];
+
+        public Stack()
+        {
+            top = -1;
+        }
+        public void Push(int Element)
+        {
+            if (top > 99)
+            {
+                Console.WriteLine("The Stack Is Full");
+            }
+            else
+            {
+                top++;
+                item[top] = Element;
+            }
+            
+        }
+        public void Pop()
+        {
+            if (top < 0)
+                Console.WriteLine("The stack Is Empty");            
+            else
+                top--;
+        }
+        public void GetTop()
+        {
+            if (top < 0)
+                Console.WriteLine("The stack Is Empty");
+            else
+                Console.WriteLine(item[top]);
+        }
+        public void Print()
+        {
+            Console.Write("Stack Is [ ");
+            for (int i = top; i >-1; i--)
+            {
+                Console.Write(item[i] + " ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
